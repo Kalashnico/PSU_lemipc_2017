6e71241c1221654353a063547f415fe212055e4e
Connor Gallimore <connor.gallimore@epitech.eu>
2018-03-26 11:01:41 +0200

    Added player struct and map defines

--- include/lemipc.h
+++ include/lemipc.h
@@ -7,6 +7,20 @@
 
 #pragma once
 
+#include <sys/types.h>
+
 #define HELP_USAGE "USAGE\n\t./lemipc PATH TEAM_NUMBER\n"
 #define HELP_PATH "\nDESCRIPTION\n\tPATH\tpath you'll give to ftok\n"
 #define HELP_TEAM_NB "\tTEAM_NUMBER\tteam number of the current champion (greater than 0)\n"
+
+#define MAP_HEIGHT 20
+#define MAP_WIDTH 20
+
+typedef struct s_player {
+	key_t key;
+	int shmid;
+	int semid;
+	int posx;
+	int posy;
+	int team;
+}	t_player;
