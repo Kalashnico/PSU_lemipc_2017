ad093b4586e165624ded48ef6a93aa552783362e
Connor Gallimore <connor.gallimore@epitech.eu>
2018-03-26 14:40:08 +0200

    Norme

--- src/map.c
+++ src/map.c
@@ -13,31 +13,33 @@
 
 int **create_map(void *memory, bool host)
 {
-    int **map = malloc(sizeof(int*) * MAP_HEIGHT * MAP_WIDTH);
+	int **map = malloc(sizeof(int*) * MAP_HEIGHT * MAP_WIDTH);
 
-    if (!map)
-        return (NULL);
-    for (int x = 0; x < MAP_WIDTH; ++x) {
-        map[x] = &((int*)memory)[MAP_HEIGHT * x];
-        if (!host)
-            memset(map[x], 0, MAP_WIDTH);
-    }
-    return (map);
+	if (!map)
+		return (NULL);
+	for (int x = 0; x < MAP_WIDTH; ++x) {
+		map[x] = &((int*)memory)[MAP_HEIGHT * x];
+		if (!host)
+			memset(map[x], 0, MAP_WIDTH);
+	}
+	return (map);
 }
 
 int **load_map(player_t *player)
 {
-    void *map;
-    int shmid = shmget(player->key, (MAP_HEIGHT * MAP_WIDTH), SHM_R | SHM_W);
+	void *map;
+	int shmid = shmget(player->key, (MAP_HEIGHT * MAP_WIDTH),
+		SHM_R | SHM_W);
 
-    if (shmid == -1) {
-        player->shmid = shmget(player->key, (MAP_HEIGHT * MAP_WIDTH), IPC_CREAT | SHM_R | SHM_W);
-        map = shmat(shmid, NULL, SHM_R | SHM_W);
-        map = create_map(map, true);
-    } else {
-        player->shmid = shmid;
-        map = shmat(shmid, NULL, SHM_R | SHM_W);
-        map = create_map(map, false);
-    }
-    return (map);
+	if (shmid == -1) {
+		player->shmid = shmget(player->key, (MAP_HEIGHT * MAP_WIDTH),
+			IPC_CREAT | SHM_R | SHM_W);
+		map = shmat(shmid, NULL, SHM_R | SHM_W);
+		map = create_map(map, true);
+	} else {
+		player->shmid = shmid;
+		map = shmat(shmid, NULL, SHM_R | SHM_W);
+		map = create_map(map, false);
+	}
+	return (map);
 }
