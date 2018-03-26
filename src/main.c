d1025688cd854f6850af23725242186f9b2c7787
Connor Gallimore <connor.gallimore@epitech.eu>
2018-03-26 10:45:04 +0200

    Fixed some stuff for compile

--- src/main.c
+++ src/main.c
@@ -6,17 +6,18 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 #include "lemipc.h"
 
 int main(int ac, char **av)
 {
-	if (ac == 1 && strcmp(av[1], "--help") == 0) {
+	if (ac == 2 && strcmp(av[1], "--help") == 0) {
 		printf("%s%s%s", HELP_USAGE, HELP_PATH, HELP_TEAM_NB);
 		return (0);
 	}
 	if (ac != 3) {
-		printf("%s\n", HELP_USAGE);
+		printf("%s", HELP_USAGE);
 		return (84);
 	}
-	// Start
+	init_components(av[1], av[2]);
 }
