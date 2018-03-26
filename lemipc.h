4a41b4ba42d015e9d385fc54ef021d7a230f4855
Nicolas Guerin <nicolas.guerin@epitech.eu>
2018-03-26 10:38:01 +0200

    Help

---
+++ lemipc.h
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2018
+** PSU_lemipc_2017
+** File description:
+** lemipc
+*/
+
+#pragma once
+
+#define HELP_USAGE "USAGE\n\t./lemipc PATH TEAM_NUMBER\n"
+#define HELP_PATH "\nDESCRIPTION\n\tPATH\tpath you'll give to ftok\n"
+#define HELP_TEAM_NB "\tTEAM_NUMBER\tteam number of the current champion (greater than 0)\n"
