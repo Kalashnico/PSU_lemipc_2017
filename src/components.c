8dbd7c1a1664981eb98b05c918c41126d303336c
Connor Gallimore <connor.gallimore@epitech.eu>
2018-03-27 13:10:06 +0200

    Free ressources

--- src/components.c
+++ src/components.c
@@ -7,7 +7,21 @@
 
 #include <stdlib.h>
 #include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
 #include "lemipc.h"
+
+void free_ressources(player_t *player, int **map)
+{
+	free(player);
+	free(map);
+}
+
+void clear_ipc(player_t *player)
+{
+	semctl(player->semid, 0, IPC_RMID);
+	shmctl(player->shmid, 0, IPC_RMID);
+}
 
 int init_components(char *path, int team)
 {
@@ -26,5 +40,7 @@ int init_components(char *path, int team)
 	create_player_sem(player);
 	put_player_on_map(player, map);
 	display_map(map);
+	clear_ipc(player);
+	free_ressources(player, map);
 	return (0);
 }
